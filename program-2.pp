1b3ec92d8fbbc988507e558cc4f6395f3d9c58a5
tutorialcode <63885381+tutorialcode@users.noreply.github.com>
2020-06-24 17:18:09 -0400

    sample program 2

---
+++ program-2.pp
@@ -0,0 +1,6 @@
+income        | What's the income?
+deduction     | What's the deduction?
+tax rate      | 0.1
+taxable income| income - deduction        | Your taxable income is __.
+income tax    | taxable income * tax rate | You have to pay __ for tax.
+take home pay | income - income tax       | And __ is your take-home pay.
